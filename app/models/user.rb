d926e3db2d9ddfa2087d4be8ac4100df998f665b
Elias Kuiter <info@elias-kuiter.de>
2013-10-02 12:56:37 +0200

    devise and http auth adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,8 @@ class User < ActiveRecord::Base
   include YoutubeConnector
   include HttpRequest
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # Include devise modules
+  devise :database_authenticatable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :full_name, :email, :password, :password_confirmation, :remember_me
