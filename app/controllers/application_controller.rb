78aa73c231c6afa502e5122d77c42483ff26a6bd
Elias Kuiter <info@elias-kuiter.de>
2013-10-02 12:12:52 +0200

    http auth removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   include YoutubeConnector
   include HttpRequest
 
-  http_basic_authenticate_with name: '***REMOVED***', password: 'elia593', realm: 'youplay'
-
   def remove_role
     params[:user].delete :role if params[user:[:role]].present?
   end
