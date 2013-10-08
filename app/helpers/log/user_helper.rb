927c1d0c236b0ee0ad7ca50d789c96267dbbe969
Elias Kuiter <info@elias-kuiter.de>
2013-10-08 11:28:48 +0200

    favorites fix

--- app/helpers/log/user_helper.rb
+++ app/helpers/log/user_helper.rb
@@ -6,7 +6,7 @@ module Log
     end
     
     def favorited_videos results, page # get watched videos by results per page and page number
-      favorites.limit(results).offset(page*results).order('created_at DESC').all.map {|f| f.video}
+      favorites.limit(results).offset(page*results).all.map {|f| f.video}.sort_by {|video| video.created_at}.reverse
     end
 
   end
