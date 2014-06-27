5dae4bc9eb583a9bbb015d5526df47fbed4415f8
Elias Kuiter <info@elias-kuiter.de>
2014-06-27 14:59:59 +0200

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,6 @@ Youplay::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+  require '../../app/lib/settings'
   config.action_mailer.default_url_options = { :host => Settings.root }
 end
