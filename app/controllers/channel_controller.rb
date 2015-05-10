84379065ac6d570f63204ccb571cf5cb6ad15ced
Elias Kuiter <info@elias-kuiter.de>
2015-05-10 10:37:44 +0200

    Channel API Fix

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -7,7 +7,11 @@ class ChannelController < ApplicationController
         parts = channel.split(':')
         provider = YouplayProvider.new provider: parts.first
         channel = YouplayChannel.new id: parts.last, provider: provider
-        channels.push username: channel.name, url: channel.url
+        begin
+          channels.push username: channel.name, url: channel.url
+        rescue
+          channels.push username: parts.last, url: "http://www.youtube.com/user/#{parts.last}"
+        end
       end
       render json: channels
     else
